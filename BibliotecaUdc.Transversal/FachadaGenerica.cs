2397acf5fd2fe2c23ba56a10e11d3bf374cd3c0e
nicolas <nicolasmahecha11@hotmail.com>
2022-12-02 08:38:11 -0500

    first commit

---
+++ BibliotecaUdc.Transversal/FachadaGenerica.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIbliotecaUDC.Negocio
+{
+    internal class FachadaGenerica<T> : IFachadaGenerica<T> where T : class
+    {
+        private readonly DataContext _db;
+        public FachadaGenerica()
+        {
+            _db = new DataContext(ConfigurationManager.ConnectionStrings["BIbliotecaUDC"].ToString())
+            {
+                DeferredLoadingEnabled = false,
+                CommandTimeout = 0
+            };
+        }
+        public bool Insert(T entity)
+        {
+            _db.GetTable<T>().InsertOnSubmit(entity);
+            _db.SubmitChanges();
+            return true;
+        }
+
+        public bool Delete(T entity)
+        {
+            _db.GetTable<T>().Attach(entity);
+            _db.Refresh(RefreshMode.KeepCurrentValues, entity);
+            _db.GetTable<T>().DeleteOnSubmit(entity);
+            _db.SubmitChanges();
+            return true;
+
+        }
+        public bool Update(T entity)
+        {
+            _db.GetTable<T>().Attach(entity);
+            _db.Refresh(RefreshMode.KeepCurrentValues, entity);
+            _db.SubmitChanges();
+            return true;
+        }
+
+        public T First(Func<T, bool> exp)
+        {
+            return _db.GetTable<T>().FirstOrDefault(exp);
+        }
+
+        public IEnumerable<T> All()
+        {
+            return _db.GetTable<T>().AsEnumerable();
+        }
+        public IEnumerable<T> FindAll(Func<T, bool> exp)
+        {
+            return _db.GetTable<T>().Where(exp);
+        }
+    }
+
+}
---
+++ BibliotecaUdc.Transversal/IFachadaGenerica.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+
+public interface IFachadaGenerica<T> where T : class
+{
+
+    bool Insert(T entity);
+    bool Delete(T entity);
+    bool Update(T entity);
+    T First(Func<T, bool> exp);
+    IEnumerable<T> All();
+    IEnumerable<T> FindAll(Func<T, bool> exp);
+}
